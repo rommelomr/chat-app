78376adbd8e551840577dff61b7758da70280868
rommelomr <rommelmontoya97@gmail.com>
2023-07-10 11:33:31 -0500

    login completed

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -37,6 +37,8 @@ export const useAuthStore = defineStore({
 
       if (error) {
         let error_info = await error.context.json();
+        console.log(error_info);
+        throw "stoppppppppp";
         let _messsage = error_info.verification
           ? error_info.verification.message
           : "Credenciales incorrectas";
